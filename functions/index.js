40e85f5211ec9fcf855bf8887cb810025d72af09
BengeeL <95111583+BengeeL@users.noreply.github.com>
2022-08-10 14:54:44 -0400

    Bug fixes + Formating
    Fix issues with the blog printing format.
    Changed visual index.html + mobile adaptation.

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@ const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
 const nodemailer = require("nodemailer");
+const mongoose = require("mongoose");
+const _ = require("lodash")
 const { google } = require("googleapis");
 const dotenv = require("dotenv");
 const cors = require("cors")({origin: true});
@@ -30,6 +32,20 @@ const OAuth2Client = new google.auth.OAuth2(
 );
 
 OAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+// DB Connect 
+mongoose.connect("mongodb+srv://admin-benjamin:BengeeL12@cluster0.fiodryj.mongodb.net/blogPostDB");
+
+// Schemas
+const postsSchema = {
+  title: String,
+  content: String
+};
+
+// Models
+const Post = mongoose.model (
+  "post", postsSchema
+);
 
 // Send email to email via nodemailer and google oauth. 
 async function sendMail (name, sender, title, content) {
@@ -108,11 +124,68 @@ app.route("/portfolio")
 ///////// BLOG /////////
 app.route("/blog")
     .get((req, res) => {
-      res.render("blog", {
-        year: currentYear,
-        pageName: "blog",
-      });
+      Post.find({}, (err, foundPosts) => {
+        if(err){
+          console.log(err);
+        } else {
+          res.render("blog", {
+            postsList: foundPosts,
+            year: currentYear,
+            pageName: "blog"
+          })
+        }  
+      })
     });
+
+// Compose article (Hidden route + ID)
+app.route("/compose")
+    .get((req,res) => {
+          res.render("compose", {
+          year: currentYear,
+          pageName: "blog"
+          })
+        })
+    .post((req,res) => {
+      const post = new Post ({
+        title: req.body.title,
+        content: req.body.post
+      })
+      post.save((err) => {
+        if (!err){
+          res.redirect("/blog");
+        };
+    })
+  });
+
+///////// POST /////////
+app.route("/blog/post/:postID")
+  .get((req,res) => {
+    const postID = req.params.postID;
+    const post = Post.findOne({_id: postID}, function(err, postFound){
+      if(!err){
+        if (post !== undefined) {
+          console.log("Match found!");
+          
+          res.render("post", {
+            postTitle: postFound.title,
+            postContent: postFound.content,
+            year: currentYear,
+            pageName: "blog"
+          })
+        }
+      } 
+
+      else {
+          console.log("No match!")
+          res.render("post", {
+            postTitle: "NO MATCH ARTICLE FOUND",
+            postContent: "Please go back to home page",
+            year: currentYear,
+            pageName: "blog"
+          });
+      };
+    });
+  });
 
 ///////// CONTACT /////////
 app.route("/contact")
@@ -134,9 +207,9 @@ app.route("/contact")
     });
 
 // PORT LISTEN 
-// const port = process.env.PORT || 3000;
-// const server = app.listen(port, () => {
-//     console.log(`server running on ${port}`)
-// })
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`server running on ${port}`)
+})
 
 exports.app = functions.https.onRequest(app);
